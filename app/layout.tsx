95d515368a9961535bb9be05517f23e123d5ad6a
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2025-03-02 13:43:22 +0200

    Add favicon links and metadata to RootLayout; remove unused favicon image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ import './globals.css';
 
 import { mainFont } from '@fonts';
 
+// export const metadata: Metadata = {
+//     description: 'Крупомол - магазин круп',
+//     title: 'Крупомол',
+// };
+
 export function generateMetadata(): Metadata {
     // const locale = await getLocale();
     // const t = await getTranslations('head');
@@ -33,6 +38,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="uk">
+            <head>
+                <link href="/favicon.ico" rel="icon" sizes="48x48" />
+                <link href="/favicon.svg" rel="icon" sizes="any" type="image/svg+xml" />
+                <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+                <meta content="Krupomol" name="apple-mobile-web-app-title" />
+            </head>
             <body className={`${mainFont.variable} flex min-h-screen flex-col antialiased`}>
                 {children}
             </body>
