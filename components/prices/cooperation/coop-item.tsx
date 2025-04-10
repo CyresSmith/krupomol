6cbd4c5adcbe141a1bf45760f14febf484c7f976
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-04-10 15:52:10 +0300

    update prices cooperation styles

--- components/prices/cooperation/coop-item.tsx
+++ components/prices/cooperation/coop-item.tsx
@@ -10,7 +10,7 @@ export const CoopItem = ({ item }: Props) => {
     const { desc, icon, title } = item;
 
     return (
-        <li className="duration-150 linear flex w-full items-center gap-3 rounded-20 bg-primary fill-transparent stroke-accent px-6 py-12 text-white transition hover:scale-[1.01] desktop:gap-4">
+        <li className="duration-150 linear flex w-full items-center gap-3 rounded-20 bg-primary fill-transparent stroke-accent px-3 py-12 text-white transition hover:scale-[1.01] desktop:gap-4 desktop:px-6">
             <Icon height={32} name={icon} width={32} />
             <div>
                 <p className="pb-4 font-title text-lg font-bold">{title}</p>
