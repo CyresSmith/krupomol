1e429c3942a7def5dee35edb7584cd2dc061a209
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-04-10 19:00:26 +0300

    fix info section styles in certification page

--- components/certification/info.tsx
+++ components/certification/info.tsx
@@ -19,7 +19,7 @@ export const Info = async () => {
 
             <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-transparent" />
             <div className="z-1 container relative">
-                <div className="w-full rounded-3xl border-[7px] border-solid border-accent px-5 py-16 tablet:w-1/2 desktop:w-1/2 desktop:py-14 desktop:pl-14">
+                <div className="w-full rounded-3xl border-[7px] border-solid border-accent px-5 py-16 tablet:w-1/2 desktop:w-1/2 desktop:px-14 desktop:py-14">
                     <Title as="h3" className="text-xl font-bold leading-[140%] text-white">
                         {t('info')}
                     </Title>
