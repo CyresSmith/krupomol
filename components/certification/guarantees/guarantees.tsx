fc1177d4ed792d063bca1ffcd24d1c69e739edfb
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-04-10 18:56:15 +0300

    add certificates-images to guarantees section

--- components/certification/guarantees/guarantees.tsx
+++ components/certification/guarantees/guarantees.tsx
@@ -2,6 +2,7 @@ import { getTranslations } from 'next-intl/server';
 
 import { GuaranteeType } from 'lib/types/garantie.types';
 
+import { CertificatesImages } from './certificates-images';
 import { Guarantee } from './guarantee';
 
 import { Section, Title } from '@components/shared';
@@ -12,8 +13,8 @@ export const Guarantees = async () => {
 
     return (
         <Section className="mt-[-64px] desktop:mt-[-96px]" variant="secondary">
-            <div className="container">
-                <div className="rounded-20 bg-primary px-3 py-12 desktop:rounded-40 desktop:px-10 desktop:py-10">
+            <div className="container relative">
+                <div className="rounded-20 bg-primary px-3 py-12 mobile:pt-[200px] tablet:pt-[100px] desktop:rounded-40 desktop:px-10 desktop:py-10">
                     <Title
                         as="h3"
                         className="mb-10 text-center text-4xl text-white desktop:text-5xl"
@@ -26,6 +27,7 @@ export const Guarantees = async () => {
                         ))}
                     </ul>
                 </div>
+                <CertificatesImages />
             </div>
         </Section>
     );
