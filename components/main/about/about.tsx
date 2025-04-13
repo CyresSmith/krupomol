33d93121560b0d1a311034e90747c72a83b88818
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-04-13 15:13:55 +0300

    fix main-about styles

--- components/main/about/about.tsx
+++ components/main/about/about.tsx
@@ -11,6 +11,7 @@ import { buttonVariants } from '@ui/button';
 import { Card } from '@ui/card';
 
 import { Icon, Section, Title } from '@components/shared';
+import { AnimatedSection } from '@components/shared/animated-section';
 import { AnimatedTextBox } from '@components/shared/animated-text-box';
 
 import { titleFont } from '@fonts';
@@ -24,9 +25,9 @@ export const About = () => {
     const products = t.raw('products') as { image: string; title: string }[];
 
     return (
-        <Section className="relative z-0">
-            <div className="container">
-                <Card className="relative mb-24 min-h-[343px] p-16 shadow-lg mobile:px-4 mobile:pb-16 mobile:pt-64">
+        <>
+            <AnimatedSection className="!pb-0">
+                <Card className="container relative min-h-[343px] p-16 shadow-lg mobile:px-4 mobile:pb-16 mobile:pt-64">
                     <div className="absolute right-0 top-1/2 h-[377px] w-[662px] -translate-y-1/2 overflow-hidden mobile:inset-x-0 mobile:top-10 mobile:h-[194px] mobile:w-full mobile:translate-y-0 tablet:w-[280px]">
                         <Image
                             alt=""
@@ -57,39 +58,42 @@ export const About = () => {
                     </AnimatedTextBox>
                     {/* </CardContent> */}
                 </Card>
-
-                <Title
-                    as="h3"
-                    className={cn(
-                        'mb-6 text-center text-4xl desktop:mb-14 desktop:text-5xl',
-                        titleFont.className
-                    )}
-                >
-                    {t('our-products')}
-                </Title>
-
-                <ul className="mb-7 grid grid-cols-1 grid-rows-[repeat(4,428px)] gap-4 tablet:grid-cols-2 tablet:grid-rows-[repeat(2,428px)] desktop:grid-cols-4 desktop:grid-rows-[360px]">
-                    {products.map(item => (
-                        <ProductCard key={item.title} {...item} />
-                    ))}
-                </ul>
-
-                <div className="flex items-center justify-end">
-                    <Link
+            </AnimatedSection>
+            <Section className="relative z-0">
+                <div className="container">
+                    <Title
+                        as="h3"
                         className={cn(
-                            'z-50 mobile:w-full',
-                            buttonVariants({ size: 'lg', variant: 'primary' })
+                            'mb-6 text-center text-4xl desktop:mb-14 desktop:text-5xl',
+                            titleFont.className
                         )}
-                        href={PRODUCTS_ROUTE}
                     >
-                        {t('link')}
+                        {t('our-products')}
+                    </Title>
 
-                        <Icon className="ml-8" name="arrow-right-top" />
-                    </Link>
+                    <ul className="mb-7 grid grid-cols-1 grid-rows-[repeat(4,428px)] gap-4 tablet:grid-cols-2 tablet:grid-rows-[repeat(2,428px)] desktop:grid-cols-4 desktop:grid-rows-[360px]">
+                        {products.map(item => (
+                            <ProductCard key={item.title} {...item} />
+                        ))}
+                    </ul>
+
+                    <div className="flex items-center justify-end">
+                        <Link
+                            className={cn(
+                                'z-50 mobile:w-full',
+                                buttonVariants({ size: 'lg', variant: 'primary' })
+                            )}
+                            href={PRODUCTS_ROUTE}
+                        >
+                            {t('link')}
+
+                            <Icon className="ml-8" name="arrow-right-top" />
+                        </Link>
+                    </div>
                 </div>
-            </div>
 
-            <span className="absolute inset-x-0 bottom-0 -z-10 h-[398px] bg-gray-color" />
-        </Section>
+                <span className="absolute inset-x-0 bottom-0 -z-10 h-[398px] bg-gray-color" />
+            </Section>
+        </>
     );
 };
