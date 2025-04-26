d2377eaf6a8b0e00e3cb5e8ad19bd3197f46c156
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-04-26 14:38:22 +0300

    update prices accordion

--- components/prices/prices-accordion/prices-accordion.tsx
+++ components/prices/prices-accordion/prices-accordion.tsx
@@ -52,31 +52,23 @@ export const PricesAccordion = () => {
     return loading ? <Loader className='fixed top-0 right-0 left-0 bottom-0' /> : productsToRender.length > 0 && (<Section className='!pb-0' variant='secondary'>
             <div className="container">
                 <div className="rounded-20 bg-primary px-4 py-4 text-background shadow-lg desktop:rounded-40 desktop:px-24 desktop:py-8">
-                    <Accordion type="multiple">
-                        <AccordionItem value="Закупівельні ціни">
-                            <AccordionTrigger className="font-title font-bold border-b-2 border-background">
-                                <Title className='mobile:text-3xl tablet:text-3xl'>Закупівельні ціни</Title>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                {productsToRender.map(product => (
-                                    <Accordion
-                                        className="mobile:pr-0 pr-12"
-                                        key={product.productName}
-                                        type="multiple"
-                                    >
-                                        <AccordionItem value={product.productName}>
-                                            <AccordionTrigger className="px-4 font-title mobile:text-lg text-xl font-bold justify-between border-b-[1px] border-background">
-                                                <div className='w-full flex mobile:flex-col mobile:gap-2 justify-between mr-6'><span>{product.productName}</span><span>{getNumberWithSpace(product.price.toFixed(2))} грн/т</span></div>
-                                            </AccordionTrigger>
-                                            <AccordionContent className='rounded-b-20 border border-t-0 border-background bg-background mobile:p-3 p-5 text-base'>
-                                                <PriceItem item={product} />
-                                            </AccordionContent>
-                                        </AccordionItem>
-                                    </Accordion>
-                                ))}
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
+                    <Title className='mobile:text-3xl tablet:text-3xl font-title font-bold border-b-2 border-background pb-4'>Закупівельні ціни</Title>
+                    {productsToRender.map(product => (
+                        <Accordion
+                            className="mobile:pr-0"
+                            key={product.productName}
+                            type="multiple"
+                        >
+                            <AccordionItem value={product.productName}>
+                                <AccordionTrigger className="px-4 font-title mobile:text-lg text-xl font-bold justify-between border-b-[1px] border-background">
+                                    <div className='w-full flex mobile:flex-col mobile:gap-2 justify-between mr-6'><span>{product.productName}</span><span>{getNumberWithSpace(product.price.toFixed(2))} грн/т</span></div>
+                                </AccordionTrigger>
+                                <AccordionContent className='rounded-b-20 border border-t-0 border-background bg-background mobile:p-3 p-5 text-base'>
+                                    <PriceItem item={product} />
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
+                    ))}
                 {data?.dates && (
                     <div>
                         <p className='font-bold mt-6 mb-4'>{dates.title}:</p>
