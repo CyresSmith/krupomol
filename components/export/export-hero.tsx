32ffca5dbf1771aefc365e3678403e329fc624d2
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2025-04-27 19:25:09 +0300

    refactor(export-hero): update translation keys for consistency

--- components/export/export-hero.tsx
+++ components/export/export-hero.tsx
@@ -5,7 +5,7 @@ import { HeroSection, Title } from '@components/shared';
 import image from '@assets/images/export-hero.jpg';
 
 export const ExportHero = async () => {
-    const t = await getTranslations('export.hero');
+    const t = await getTranslations('export');
 
     return (
         <HeroSection image={image}>
@@ -13,10 +13,10 @@ export const ExportHero = async () => {
                 as="h1"
                 className="mb-[20px] text-left text-[44px] font-bold leading-normal text-background desktop:text-6xl desktop:leading-normal"
             >
-                {t('title')}
+                {t('hero.title')}
             </Title>
             <p className="text-sm text-white drop-shadow-lg desktop:w-1/2 desktop:text-base">
-                {t('desc')}
+                {t('hero.desc')}
             </p>
         </HeroSection>
     );
