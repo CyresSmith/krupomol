029d9c4046f46c752c7155de980119384140c8ba
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-04-28 15:21:18 +0300

    add email template

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -43,9 +43,40 @@ export async function POST(req: NextRequest) {
 
         await transporter.sendMail({
             from: user,
+            html: `
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Responsive Email Template</title>
+    </head>
+    <body style="font-family: 'Inter', sans-serif">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td align="center" style="padding: 20px;">
+                    <table class="content" width="600" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border: 1px solid #072C73;">
+                        <tr>
+                            <td class="header" style="background-color: #072C73; padding: 20px; text-align: center; color: white; font-size: 24px;">
+                            Тема: ${theme}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="body" style="padding: 20px; text-align: left; font-size: 16px; line-height: 1.6;">
+                            <p style="font-weight: bold; color: black">Від: <span style="font-weight: 400">${name}</span></p>
+                            <p style="font-weight: bold; color: black">Телефон: <span style="font-weight: 400">${phone}</span></p>
+                            <p style="font-weight: bold; color: black">Email: <span style="font-weight: 400">${email}</span></p>
+                            <p style="font-weight: bold; color: black">Повідомлення: <span style="font-weight: 400">${message}</span></p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
+`,
             replyTo: email,
-            subject: `${theme}`,
-            text: `Отримано повідомлення від ${name} - ${phone}: ${message}`,
+            subject: 'Повідомлення з сайту Крупомол',
             to: receiver,
         });
 
@@ -56,3 +87,29 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ error: true, message: error.message }, { status: 500 });
     }
 }
+
+// `
+// <body style="font-family: 'Inter', sans-serif">
+//     <table width="100%" border="0" cellspacing="0" cellpadding="0">
+//         <tr>
+//             <td align="center" style="padding: 20px;">
+//                 <table class="content" width="600" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border: 1px solid #cccccc;">
+//                     <!-- Header -->
+//                     <tr>
+//                         <td class="header" style="background-color: #345C72; padding: 40px; text-align: center; color: white; font-size: 24px;">
+//                         ${}
+//                         </td>
+//                     </tr>
+//                     <tr>
+//                         <td class="body" style="padding: 40px; text-align: left; font-size: 16px; line-height: 1.6;">
+//                         ${}
+//                         <br><br>
+//                         ${}
+//                         </td>
+//                     </tr>
+//                 </table>
+//             </td>
+//         </tr>
+//     </table>
+// </body>
+// `;
