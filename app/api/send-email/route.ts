41172700dd4911f6c747bfd55f8c11dc24bbc845
CyresSmith <madnezzz5@gmail.com>
2025-05-20 10:30:52 +0300

    fix: update type assertion for request body and improve email HTML structure

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ error: true, message: 'Check env!' }, { status: 400 });
     }
 
-    const body = (await req.json()) as unknown;
+    const body = (await req.json()) as Record<string, unknown>;
 
     if (typeof body !== 'object' || body === null) {
         return NextResponse.json(
@@ -25,7 +25,7 @@ export async function POST(req: NextRequest) {
         );
     }
 
-    const { token, ...data } = body as Record<string, unknown>;
+    const { token, ...data } = body;
 
     const secret = process.env['RECAPTCHA_SECRET_KEY'];
 
@@ -68,37 +68,37 @@ export async function POST(req: NextRequest) {
         await transporter.sendMail({
             from: user,
             html: `
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Responsive Email Template</title>
-    </head>
-    <body style="font-family: 'Inter', sans-serif">
-        <table width="100%" border="0" cellspacing="0" cellpadding="0">
-            <tr>
-                <td align="center" style="padding: 20px;">
-                    <table class="content" width="600" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border: 1px solid #072C73;">
-                        <tr>
-                            <td class="header" style="background-color: #072C73; padding: 20px; text-align: center; color: white; font-size: 24px;">
-                            Тема: ${theme}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="body" style="padding: 20px; text-align: left; font-size: 16px; line-height: 1.6;">
-                            <p style="font-weight: bold; color: black">Від: <span style="font-weight: 400">${name}</span></p>
-                            <p style="font-weight: bold; color: black">Телефон: <span style="font-weight: 400">${phone}</span></p>
-                            <p style="font-weight: bold; color: black">Email: <span style="font-weight: 400">${email}</span></p>
-                            <p style="font-weight: bold; color: black">Повідомлення: <span style="font-weight: 400">${message}</span></p>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
-    </body>
-</html>
-`,
+                    <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>Responsive Email Template</title>
+                        </head>
+                        <body style="font-family: 'Inter', sans-serif">
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td align="center" style="padding: 20px;">
+                                        <table class="content" width="600" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border: 1px solid #072C73;">
+                                            <tr>
+                                                <td class="header" style="background-color: #072C73; padding: 20px; text-align: center; color: white; font-size: 24px;">
+                                                Тема: ${theme}
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td class="body" style="padding: 20px; text-align: left; font-size: 16px; line-height: 1.6;">
+                                                <p style="font-weight: bold; color: black">Від: <span style="font-weight: 400">${name}</span></p>
+                                                <p style="font-weight: bold; color: black">Телефон: <span style="font-weight: 400">${phone}</span></p>
+                                                <p style="font-weight: bold; color: black">Email: <span style="font-weight: 400">${email}</span></p>
+                                                <p style="font-weight: bold; color: black">Повідомлення: <span style="font-weight: 400">${message}</span></p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </body>
+                    </html>
+                    `,
             replyTo: email,
             subject: 'Повідомлення з сайту Крупомол',
             to: receiver,
